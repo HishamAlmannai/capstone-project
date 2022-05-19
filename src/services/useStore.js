ee6ec9313b059103cbe989421020673e89fd109f
Hisham Almannai <hisham@gmx.de>
2022-05-19 18:10:06 +0200

    Add persist to zustand

--- src/services/useStore.js
+++ src/services/useStore.js
@@ -1,34 +1,44 @@
 import create from 'zustand';
 import { nanoid } from 'nanoid';
+import { persist } from 'zustand/middleware';
 
-const useStore = create(set => ({
-	tasks: [
-		{ id: nanoid(), name: 'The tasks are displayed, labeled by "ToDo:"', done: true },
-		{
-			id: nanoid(),
-			name: 'If a task has already been done, the checkbox should be checked',
-			done: false,
-		},
-		{ id: nanoid(), name: 'Each task should be visually separated', done: true },
-		{ id: nanoid(), name: "Each task should display it's own checkbox", done: true },
+const useStore = create(
+	persist(
+		set => ({
+			tasks: [
+				{ id: nanoid(), name: 'The tasks are displayed, labeled by "ToDo:"', done: true },
+				{
+					id: nanoid(),
+					name: 'If a task has already been done, the checkbox should be checked',
+					done: false,
+				},
+				{ id: nanoid(), name: 'Each task should be visually separated', done: true },
+				{ id: nanoid(), name: "Each task should display it's own checkbox", done: true },
 
-		{ id: nanoid(), name: 'All tasks should be displayed in vertical order', done: true },
-	],
+				{
+					id: nanoid(),
+					name: 'All tasks should be displayed in vertical order',
+					done: true,
+				},
+			],
 
-	addTask: task => {
-		set(state => {
-			return {
-				tasks: [{ id: nanoid(), name: task, done: false }, ...state.tasks],
-			};
-		});
-	},
-	deleteTask: id => {
-		set(state => {
-			return {
-				tasks: state.tasks.filter(dask => dask.id !== id),
-			};
-		});
-	},
-}));
+			addTask: task => {
+				set(state => {
+					return {
+						tasks: [{ id: nanoid(), name: task, done: false }, ...state.tasks],
+					};
+				});
+			},
+			deleteTask: id => {
+				set(state => {
+					return {
+						tasks: state.tasks.filter(dask => dask.id !== id),
+					};
+				});
+			},
+		}),
+		{ name: 'local tasks' }
+	)
+);
 
 export default useStore;
