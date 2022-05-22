3ebfca08209b713a795e4afe88fa0e3882c26160
Hisham Almannai <hisham@gmx.de>
2022-05-22 21:30:08 +0200

    Add button and filter

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import useStore from '../../services/useStore';
+import Button from '../Button/Button';
 import Task from '../Task/Task';
 
 export default function TaskList() {
 	const tasks = useStore(state => state.tasks);
-
+	const archiveTasks = useStore(state => state.archiveTasks);
+	const archivedTasks = tasks.filter(task => task.archive === false);
 	return (
-		<ul>
-			{tasks.map(task => (
-				<Task key={task.id} task={task} />
-			))}
-		</ul>
+		<>
+			<ul>
+				{archivedTasks.map(task => (
+					<Task key={task.id} task={task} />
+				))}
+			</ul>
+			<Button
+				onClick={() => {
+					archiveTasks();
+				}}
+			>
+				Archive checked
+			</Button>
+		</>
 	);
 }
