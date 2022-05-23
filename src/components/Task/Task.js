1b6f3da299cc87b9e3f304efd26a7771db6d85ed
Hisham Almannai <hisham@gmx.de>
2022-05-23 16:24:25 +0200

    Add button

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StyledListItem from '../../../styles/ListItem.styled';
 import useStore from '../../services/useStore';
 import Button from '../Button/Button';
 import Checkbox from '../Checkbox/Checkbox';
+import Form from '../Form/Form';
 
 export default function Task(props) {
 	const deleteTask = useStore(state => state.deleteTask);
+	const [editMode, setEditMode] = useState(false);
 
 	return (
 		<StyledListItem role="listitem">
 			<Checkbox role="checkbox" task={props.task} />
-			<span>{props.task.name}</span>
+			{editMode ? (
+				<Form />
+			) : (
+				<>
+					<span>{props.task.name}</span>
+					<Button
+						name="Button"
+						onClick={() => {
+							setEditMode(true);
+						}}
+					>
+						edit
+					</Button>
+				</>
+			)}
 			<Button
 				name="Button"
 				onClick={() => {
