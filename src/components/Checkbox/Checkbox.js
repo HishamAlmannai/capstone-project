9f0f274a2155954cbfdfbcbbf24acf5ca9903a8e
Hisham Almannai <hisham@gmx.de>
2022-05-29 13:49:17 +0200

    Destructure props.task

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -2,10 +2,7 @@ import React from 'react';
 import StyledCheckbox from '../../../styles/Checkbox.styled';
 import useStore from '../../services/useStore';
 
-export default function Checkbox(props) {
-	const id = props.task.id;
-	const done = props.task.done;
-
+export default function Checkbox({ id, done }) {
 	const checkTask = useStore(state => state.checkTask);
 
 	return (
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,27 +1,39 @@
 import { format } from 'date-fns';
 import React, { useState } from 'react';
+import StyledCheckbox from '../../../styles/Checkbox.styled';
 import StyledListItem from '../../../styles/ListItem.styled';
 import useStore from '../../services/useStore';
 import Button from '../Button/Button';
-import Checkbox from '../Checkbox/Checkbox';
 import Form from '../Form/Form';
 
 export default function Task(props) {
+	const {
+		task: { id, name, done, dueDate },
+	} = props;
 	const deleteTask = useStore(state => state.deleteTask);
 	const [editMode, setEditMode] = useState('');
 	const exitEditMode = () => {
 		setEditMode(false);
 	};
-	const formatDueDate = format(new Date(props.task.dueDate), 'dd/MM/yyyy');
+	const checkTask = useStore(state => state.checkTask);
+
+	const formatDueDate = format(new Date(dueDate), 'dd/MM/yyyy');
 
 	return (
 		<StyledListItem role="listitem">
-			<Checkbox task={props.task} />
+			<StyledCheckbox
+				type="checkbox"
+				name="checkbox"
+				checked={done}
+				onChange={() => {
+					checkTask(id);
+				}}
+			/>
 			{editMode ? (
-				<Form editMode id={props.task.id} exitEditMode={exitEditMode} />
+				<Form editMode id={id} exitEditMode={exitEditMode} />
 			) : (
 				<>
-					<span>{props.task.name}</span>
+					<span>{name}</span>
 					<span>{formatDueDate}</span>
 					<Button
 						name="edit"
@@ -36,12 +48,11 @@ export default function Task(props) {
 			<Button
 				name="delete"
 				onClick={() => {
-					deleteTask(props.task.id);
+					deleteTask(id);
 				}}
 			>
 				Delete
 			</Button>
-
 			<hr />
 		</StyledListItem>
 	);
