d6d3c89b5fac0ffee0e31de82af6f0fcaa50481d
Hisham Almannai <hisham@gmx.de>
2022-06-07 17:51:05 +0200

    Add Graph input storybook

--- src/components/Graph/Graph.stories.js
+++ src/components/Graph/Graph.stories.js
@@ -1,13 +1,16 @@
+import { CategoryScale, Chart, LinearScale, LineElement, PointElement } from 'chart.js';
 import React from 'react';
-import Graph from './Graph';
+import { Line } from 'react-chartjs-2';
+
+Chart.register(LinearScale, CategoryScale, PointElement, LineElement);
 
 const props = {
 	data: {
-		labels: [1, 2, 3],
+		labels: [1, 2, 3, 3, 555, 4],
 		datasets: [
 			{
 				label: '',
-				data: [2, 3, 1],
+				data: [2, 3, 1, 4, 6, 4],
 				backgroundColor: ['rgba(255, 99, 132, 0.2)'],
 				borderColor: ['rgba(54, 162, 235, 1)'],
 				borderWidth: 1,
@@ -41,5 +44,5 @@ export default {
 };
 
 export function Default() {
-	return <Graph {...props} />;
+	return <Line {...props} />;
 }
