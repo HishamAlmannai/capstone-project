f1ed52ba624c3f8a65c903b6482f7949e643cbaa
Hisham Almannai <hisham@gmx.de>
2022-11-09 14:37:42 +0100

    add String() somewhere else

--- src/lib/db/database.js
+++ src/lib/db/database.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 
-const url = String(process.env.DB_CONNECTION);
+const url = process.env.DB_CONNECTION;
 
 export async function dbConnect() {
 	try {
-		await mongoose.connect(url, {
+		await mongoose.connect(String(url), {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
 		console.log('Connected to MongoDB');
+		console.log(typeof url);
 	} catch (error) {
 		console.error('ERROR, could not connect', error.message);
 	}
