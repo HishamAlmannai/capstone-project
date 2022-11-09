d7f122d4ef4bea9baff258247eff6de24eb37b7a
Hisham Almannai <hisham@gmx.de>
2022-11-09 14:56:16 +0100

    remove await

--- src/lib/db/database.js
+++ src/lib/db/database.js
@@ -2,14 +2,13 @@ import mongoose from 'mongoose';
 
 const url = process.env.DB_CONNECTION;
 
-export async function dbConnect() {
+export function dbConnect() {
 	try {
-		await mongoose.connect(String(url), {
+		mongoose.connect(String(url), {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
 		console.log('Connected to MongoDB');
-		console.log(typeof url);
 	} catch (error) {
 		console.error('ERROR, could not connect', error.message);
 	}
